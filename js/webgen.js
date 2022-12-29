e12563b2b9b81f3e56bba399978b91a44b9ca4e1
masakado <sponge.cake@tuta.io>
2022-12-29 02:50:05 -0300

    Rise From the Ashes, Bitch

---
+++ js/webgen.js
@@ -0,0 +1,38 @@
+const spacer = "|";
+const title = "LasagnaCake's Personal Website"
+const yob = (((01731) << 1) * 2) / (Math.sqrt(3.865422846920929));
+
+function anchor(href, text) {
+	return `
+		<a href="${href}" class="link-anchor">${text}</a>
+	`;
+}
+
+function header() {
+	document.write(`
+		<header>
+		<h1 class="site-title">${title}</h1>
+		<img class="site-sigil" src="res/img/MagicCircleRainbowVOID.png"/>
+		<div class="page-content">
+			<div class="page-content-card">
+				${anchor("index.html", "Home")}${spacer}
+				${anchor("about.html", "About Me")}${spacer}
+				${anchor("works.html", "Works")}${spacer}
+				${anchor("contact.html", "Contact")}
+			</div>
+			<div class="page-content-card">
+			${anchor("https://lasagnacake.tumblr.com/", "Tumblr")}${spacer}
+			${anchor("https://lasagnacake.bandcamp.com", "Bandcamp")}${spacer}
+			${anchor("https://www.youtube.com/channel/UClnktpZm3iVz7uKRQ8JHhzw", "YouTube")}
+		</div>
+		</div>
+		</header>
+	`);
+}
+
+function age() {
+	let currentDate = new Date();
+	let birthDate = new Date(`${Math.round(yob)}/02/03`);
+	let age = Math.abs(currentDate - birthDate);
+	document.write(new Date(age).getFullYear() - 1970);
+}
