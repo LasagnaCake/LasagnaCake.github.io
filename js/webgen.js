be192a58ff05ea1c79640f62c8fe24d2758f7530
masakado <sponge.cake@tuta.io>
2023-01-26 13:47:47 -0300

    Update webgen.js

--- js/webgen.js
+++ js/webgen.js
@@ -2,9 +2,23 @@ const spacer		= " ";
 const title			= "LasagnaCake's Personal Website";
 const sigilImage	= "res/img/MagicCircleRainbowPolarVOID.png";
 
+const link = {
+	"tumblr":	"https://lasagnacake.tumblr.com/",
+	"bandcamp":	"https://lasagnacake.bandcamp.com",
+	"youtube":	"https://www.youtube.com/channel/UClnktpZm3iVz7uKRQ8JHhzw"
+};
+
 const present = (...text) => document.write(text);
 
 const yob = (((01731) << 1) * 2) / (Math.sqrt(3.865422846920929));
+
+function anchor(href, text) {
+	return `<a href="${href}" class="link-anchor">[${text}]</a>`;
+}
+
+function social(site) {
+	return anchor(link[site.toLowerCase()], site);
+}
 
 const navigation = `
 	<div class="page-content">
@@ -14,18 +28,12 @@ const navigation = `
 			${anchor("works.html", "Works")}
 		</div>
 		<div class="page-content-card small-footprint">
-		${anchor("https://lasagnacake.tumblr.com/", "Tumblr")}${spacer}
-		${anchor("https://lasagnacake.bandcamp.com", "Bandcamp")}${spacer}
-		${anchor("https://www.youtube.com/channel/UClnktpZm3iVz7uKRQ8JHhzw", "YouTube")}
+		${social("Tumblr")}${spacer}
+		${social("Bandcamp")}${spacer}
+		${social("YouTube")}
 		</div>
 	</div>
 `;
-
-function anchor(href, text) {
-	return `
-		<a href="${href}" class="link-anchor">[${text}]</a>
-	`;
-}
 
 function header() {
 	present(`
