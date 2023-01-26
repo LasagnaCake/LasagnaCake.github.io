bc778e39633c5de9347cde441372924859a4468e
masakado <sponge.cake@tuta.io>
2023-01-25 22:32:57 -0300

    Masakadon't

--- js/webgen.js
+++ js/webgen.js
@@ -1,53 +1,44 @@
-const spacer		= "|";
+const spacer		= "";
 const title			= "LasagnaCake's Personal Website";
 const sigilImage	= "res/img/MagicCircleRainbowPolarVOID.png";
 
+const present = (...text) => document.write(text);
+
 const yob = (((01731) << 1) * 2) / (Math.sqrt(3.865422846920929));
+
+const navigation = `
+	<div class="page-content">
+		<div class="page-content-card small-footprint" style="margin-right: 0.75vw;">
+			${anchor("index.html", "Home")}${spacer}
+			${anchor("about.html", "About Me")}${spacer}
+			${anchor("works.html", "Works")}
+		</div>
+		<div class="page-content-card small-footprint">
+		${anchor("https://lasagnacake.tumblr.com/", "Tumblr")}${spacer}
+		${anchor("https://lasagnacake.bandcamp.com", "Bandcamp")}${spacer}
+		${anchor("https://www.youtube.com/channel/UClnktpZm3iVz7uKRQ8JHhzw", "YouTube")}
+		</div>
+	</div>
+`;
 
 function anchor(href, text) {
 	return `
-		<a href="${href}" class="link-anchor">${text}</a>
+		<a href="${href}" class="link-anchor">[${text}]</a>
 	`;
 }
 
 function header() {
-	document.write(`
+	present(`
 		<header>
 			<h1 class="site-title">${title}</h1>
 			<img class="site-sigil" src="${sigilImage}"/>
-			<div class="page-content">
-				<div class="page-content-card small-footprint" style="margin-right: 0.75vw;">
-					${anchor("index.html", "Home")}${spacer}
-					${anchor("about.html", "About Me")}${spacer}
-					${anchor("works.html", "Works")}
-				</div>
-				<div class="page-content-card small-footprint">
-				${anchor("https://lasagnacake.tumblr.com/", "Tumblr")}${spacer}
-				${anchor("https://lasagnacake.bandcamp.com", "Bandcamp")}${spacer}
-				${anchor("https://www.youtube.com/channel/UClnktpZm3iVz7uKRQ8JHhzw", "YouTube")}
-				</div>
-			</div>
+			${navigation}
 		</header>
 	`);
 }
 
 function footer() {
-	document.write(`
-	<footer>
-		<div class="page-content">
-			<div class="page-content-card small-footprint" style="margin-right: 0.75vw;">
-				${anchor("index.html", "Home")}${spacer}
-				${anchor("about.html", "About Me")}${spacer}
-				${anchor("works.html", "Works")}
-			</div>
-			<div class="page-content-card small-footprint">
-			${anchor("https://lasagnacake.tumblr.com/", "Tumblr")}${spacer}
-			${anchor("https://lasagnacake.bandcamp.com", "Bandcamp")}${spacer}
-			${anchor("https://www.youtube.com/channel/UClnktpZm3iVz7uKRQ8JHhzw", "YouTube")}
-			</div>
-		</div>
-	</footer>
-	`);
+	present(`<footer>${navigation}</footer>`);
 }
 
 function age() {
