e1d65cd88321f28374ae053f565881ad3dca15ba
masakado <sponge.cake@tuta.io>
2023-02-03 21:34:36 -0300

    More Webbed Site!

--- js/webgen.js
+++ js/webgen.js
@@ -1,16 +1,19 @@
-const spacer		= " ";
+"use strict";
+
+const spacer		= "<br>";
 const title			= "LasagnaCake's Personal Website";
 const sigilImage	= "res/img/MagicCircleRainbowPolarVOID.png";
 
 const link = {
 	"tumblr":	"https://lasagnacake.tumblr.com/",
 	"bandcamp":	"https://lasagnacake.bandcamp.com",
-	"youtube":	"https://www.youtube.com/channel/UClnktpZm3iVz7uKRQ8JHhzw"
+	"youtube":	"https://www.youtube.com/channel/UClnktpZm3iVz7uKRQ8JHhzw",
+	"blog":		"https://lasagnacake.neocities.org/"
 };
 
 const present = (...text) => document.write(text);
 
-const yob = (((01731) << 1) * 2) / (Math.sqrt(3.865422846920929));
+const yob = (((0o1731) << 1) * 2) / (Math.sqrt(3.865422846920929));
 
 function anchor(href, text) {
 	return `<a href="${href}" class="link-anchor">[${text}]</a>`;
