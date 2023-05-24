9ede529fe820a90468a4298d735a72e414f50fab
LasagnaCake <sponge.cake@tuta.io>
2023-05-24 11:48:27 -0300

    Modals!

--- js/webgen.js
+++ js/webgen.js
@@ -11,3 +11,49 @@ function age() {
 	let age = Math.abs(currentDate - birthDate);
 	agespan.innerText = new Date(age).getFullYear() - 1970;
 }
+
+const Modal = {
+	"yesNo": (id, text, onYes = ()=>{}, onNo = ()=>{}) => {
+		let modal = document.createElement("dialog");
+		let buttons = document.createElement("div");
+		let yes = document.createElement("button");
+		let no = document.createElement("button");
+		modal.id = id;
+		modal.innerText = text;
+		yes.innerText = "Yes";
+		no.innerText = "No";
+		yes.onclick = (e) => {onYes(e); modal.close();};
+		no.onclick = (e) => {onNo(e); modal.close();};
+		modal.onclose = (e) => {modal.remove();};
+		buttons.appendChild(yes);
+		buttons.appendChild(no);
+		modal.appendChild(buttons);
+		document.body.appendChild(modal);
+		modal.showModal();
+		return modal;
+	},
+	"yesNoCancel": (id, text, onYes = ()=>{}, onNo = ()=>{}, onCancel = ()=>{}) => {
+		let modal = document.createElement("dialog");
+		let buttons = document.createElement("div");
+		let yes = document.createElement("button");
+		let no = document.createElement("button");
+		let cancel = document.createElement("button");
+		modal.id = id;
+		modal.innerText = text;
+		buttons.className = "modal-buttons";
+		yes.innerText = "Yes";
+		no.innerText = "No";
+		cancel.innerText = "Cancel";
+		yes.onclick = (e) => {onYes(e); modal.close();};
+		no.onclick = (e) => {onNo(e); modal.close();};
+		cancel.onclick = (e) => {onCancel(e); modal.close();};
+		modal.onclose = (e) => {modal.remove();};
+		buttons.appendChild(yes);
+		buttons.appendChild(no);
+		buttons.appendChild(cancel);
+		modal.appendChild(buttons);
+		document.body.appendChild(modal);
+		modal.showModal();
+		return modal;
+	}
+};
