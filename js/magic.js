3352ff14de0a33a03c20dce906552f4de75927d6
LasagnaCake <sponge.cake@tuta.io>
2024-06-08 18:37:09 -0300

    Some Reworking

--- js/magic.js
+++ js/magic.js
@@ -1,13 +1,1 @@
 "use strict";
-
-const e = 3.865422846920929;
-
-const y = (((0o1731) << 1) * 2);
-
-function age() {
-	const decy = (a, b) => Math.round(a / Math.sqrt(b));
-	let currentDate = new Date();
-	let birthDate = new Date(`${decy(y,e)}/${0x100>>7}/${0o300>>6}`);
-	let age = Math.abs(currentDate - birthDate);
-	agespan.innerText = new Date(age).getFullYear() - 1970;
-}
